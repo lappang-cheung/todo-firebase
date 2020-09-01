ffa6910463180c173a01ac2ece87310310dc135b
Leo Cheung <lappangcheung@gmail.com>
2020-08-31 23:22:04 -0400

    delete user

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,19 +1,22 @@
 // Auth Services
-export const AUTH_START = 'AUTH_START'
-export const AUTH_END = 'AUTH_END'
-export const AUTH_SUCCESS = 'AUTH_SUCCESS'
-export const AUTH_FAIL = 'AUTH_FAIL'
+export const AUTH_START = 'AUTH_START';
+export const AUTH_END = 'AUTH_END';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_FAIL = 'AUTH_FAIL';
 // Clean Up
-export const CLEAN_UP = 'CLEAN_UP'
+export const CLEAN_UP = 'CLEAN_UP';
 // Verify Email
-export const VERIFY_START = 'VERIFY_START'
-export const VERIFY_SUCCESS = 'VERIFY_SUCCESS'
-export const VERIFY_FAIL = 'VERIFY_FAIL'
+export const VERIFY_START = 'VERIFY_START';
+export const VERIFY_SUCCESS = 'VERIFY_SUCCESS';
+export const VERIFY_FAIL = 'VERIFY_FAIL';
 // Recover Password
-export const RECOVERY_START = 'RECOVERY_START'
-export const RECOVERY_SUCCESS = 'RECOVERY_SUCCESS'
-export const RECOVERY_FAIL = 'RECOVERY_FAIL'
+export const RECOVERY_START = 'RECOVERY_START';
+export const RECOVERY_SUCCESS = 'RECOVERY_SUCCESS';
+export const RECOVERY_FAIL = 'RECOVERY_FAIL';
 // Edit Profile
-export const PROFILE_EDIT_START = 'PROFILE_EDIT_START'
-export const PROFILE_EDIT_SUCCESS = 'PROFILE_EDIT_SUCCESS'
-export const PROFILE_EDIT_FAIL = 'PROFILE_EDIT_FAIL'
+export const PROFILE_EDIT_START = 'PROFILE_EDIT_START';
+export const PROFILE_EDIT_SUCCESS = 'PROFILE_EDIT_SUCCESS';
+export const PROFILE_EDIT_FAIL = 'PROFILE_EDIT_FAIL';
+// Delete Profile
+export const DELETE_START = 'DELETE_START';
+export const DELETE_FAIL = 'DELETE_FAIL';
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,1 +1,1 @@
-export { signUp, signIn, signOut, clean, verifyEmail, recoverPassword, editProfile } from './authActions';
+export { signUp, signIn, signOut, clean, verifyEmail, recoverPassword, editProfile, deleteUser } from './authActions';
