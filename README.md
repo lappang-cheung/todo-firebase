0633ef1d0e737e22a941d47597ce76d07fa38342
Leo Cheung <lappangcheung@gmail.com>
2021-01-31 17:03:12 -0500

    Updated the readme

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-# Testing Project
+# Audio Support
 
-React, Firebase & Redux 
+## Context
+This application was for early experiment for learning Firebase and Redux with user email verification. 
+
+## Installation Steps
+
+To run this application please do the following:
+
+1. In the env file please, add
+```REACT_APP_APIKEY=AIzaSyCR1GVCD63yKvu41hjuWiZk731-K2g6co8```
+
+2. You can use the following install the project
+```
+yarn install || npm run install
+yarn start   || npm run start
+```
+
+3. Register yourself and valid your email
+
+4. Please go to do the Todo Tab and you shall see the audio support with the logo.
--- src/containers/Todos/Todos.js
+++ src/containers/Todos/Todos.js
@@ -1,9 +1,685 @@
-import React from 'react'
+import React, {Component} from 'react';
+import styled from 'styled-components';
 
-export default function Todos() {
-    return (
-        <div>
-            This is the todo page
+// Style components
+const AudioWrapper = styled.div`
+    position: fixed;
+    bottom: 0;
+    right: 0;
+
+    @media ${props => props.theme.mediaQueries.smallest} {
+        width: 250px;
+    }
+`
+
+const AudioButton = styled.button`
+    float: right;
+    background-color: inherit;
+    border: none;
+    color: grey;
+    text-transform: uppercase;
+    font-weight: bold;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 8px;
+`
+
+const AudioPlayer = styled.audio`
+    // Media query for small screens
+    @media ${props => props.theme.mediaQueries.small} {
+        width: 250px;
+    }
+
+    @media ${props => props.theme.mediaQueries.smallest} {
+        width: 250px;
+    }
+    
+`
+
+class Todos extends Component {
+
+    //  Component state
+    state = {
+        play: false
+    }
+
+    componentWillReceiveProps() {
+        this.setState({
+            play: true 
+        })
+    }
+
+    // Play logic
+    playing = () => {
+
+        const { play } = this.state
+
+        console.log(this.audio)
+
+        if (play) {
+        this.setState({ play: false });
+        this.audio.pause();
+        } else {
+        this.setState({ play: true });
+        this.audio.play();
+        }
+    }
+
+
+    render() {
+
+        const { play } = this.state
+
+        return <div
+            style={{
+                width: '50vw',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center'
+            }}
+        >
+            <h2
+                style={{
+                    textAlign: 'center'
+                }}
+            >To do List</h2>
+            <ul>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li><li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+                <li>
+                    Hello world
+                </li>
+            </ul>
+            <AudioWrapper>
+            {
+                !play &&
+                <AudioButton
+                    onClick={this.playing}>
+                    <span
+                        style={{
+                            backgroundColor: 'lightgrey',
+                            borderRadius: '50%',
+                            height: 40,
+                            width: 40,
+                            marginRight: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}
+                    >
+                    <i 
+                    
+                        style={{
+                            color: '#696969'
+                        }}
+                        className="fas fa-play" 
+                    />
+                    </span>
+                    Listen to story
+                </AudioButton>
+            }
+            <AudioPlayer src="https://s3.amazonaws.com/mobile.engineering.com/audio/19559.mp3"
+                style={{
+                    display: !play ? 'none' : 'inline'
+                }}
+                ref={(audio) => { this.audio = audio }}
+                controls
+            />
+            </AudioWrapper>
         </div>
-    )
+    }
 }
+
+export default Todos
