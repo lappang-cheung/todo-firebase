68a2666b9c5b02a479b9afc031ec1ec8116a517f
Leo Cheung <lappangcheung@gmail.com>
2021-02-22 02:56:22 -0500

    Add todos udpated

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import { firebaseReducer } from 'react-redux-firebase';
 
 import authReducer from './authReducer';
+import todosReducers from './todosReducer'
 
 export default combineReducers({
     auth: authReducer,
+    todos: todosReducers,
     firebase: firebaseReducer
 })
